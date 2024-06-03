53ff70be34d4526edfff044d667e66834039e3c5
Tranken <yuehengh4002@gmail.com>
2024-06-02 20:16:01 -0700

    changed to static camera

--- italy/Assets/Scripts/EndMenu.cs
+++ italy/Assets/Scripts/EndMenu.cs
@@ -2,9 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraMovement : MonoBehaviour
+public class EndMenu : MonoBehaviour
 {
-    public GameObject target;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +13,6 @@ public class CameraMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // transform.position = new Vector3(target.transform.position.x, target.transform.position.y, -10);
+        
     }
 }
